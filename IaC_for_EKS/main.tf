9a4bc1519caa78dccf2950fd07f97dda24d5100a
Birendra Kumar <84340734+Birendrakum@users.noreply.github.com>
2026-04-04 08:03:35 +0530

    Update main.tf

--- IaC_for_EKS/main.tf
+++ IaC_for_EKS/main.tf
@@ -8,11 +8,11 @@ module "vpc" {
   version = "5.0.0"
 
   name = "eks-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "10.1.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  public_subnets  = ["10.1.1.0/24", "10.1.2.0/24"]
+  private_subnets = ["10.1.3.0/24", "10.1.4.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
